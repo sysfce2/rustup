980a552268f5714afe2730485d111173f8995e9f
rami3l <rami3l@outlook.com>
2024-05-24 21:48:55 +0800

    fix(dist/triple): use segments generated from `platforms` at comptime

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
-use std::env;
+use std::{
+    collections::BTreeSet,
+    env, fs,
+    io::{self, Write as _},
+    path::Path,
+};
 
 use platforms::Platform;
 
@@ -10,6 +15,96 @@ fn from_build() -> Result<String, String> {
     } else {
         Err(triple)
     }
+}
+
+/// Generates the lists of known target architectures, OSes and environments.
+fn generate_known_triples() -> io::Result<()> {
+    /// Parses the given triple into 3 parts (target architecture, OS and environment).
+    ///
+    /// # Discussion
+    ///
+    /// The current model of target triples in Rustup requires some non-code knowledge to correctly generate the list.
+    /// For example, the parsing results of two 2-dash triples can be different:
+    ///
+    /// ```jsonc
+    /// { arch: aarch64, os: linux, env: android }
+    /// { arch: aarch64, os: unknown-freebsd}
+    /// ```
+    ///
+    /// Thus, the following parsing scheme is used:
+    ///
+    /// ```jsonc
+    /// // for `x-y`
+    /// { arch: x, os: y }
+    ///
+    /// // special case for `x-y-w` where `y` is `none` or `linux`
+    /// // e.g. `thumbv4t-none-eabi`, `i686-linux-android`
+    /// // (should've been called `x-unknown-y-w`, but alas)
+    /// { arch: x, os: y, env: w }
+    ///
+    /// // for `x-y-z`
+    /// { arch: x, os: y-z }
+    ///
+    /// // for `x-y-z-w`
+    /// { arch: x, os: y-z, env: w }
+    /// ```
+    fn parse_triple(triple: &str) -> Option<(&str, &str, &str)> {
+        match triple.split('-').collect::<Vec<_>>()[..] {
+            [arch, os] => Some((arch, os, "")),
+            [arch, os @ ("none" | "linux"), env] => Some((arch, os, env)),
+            [arch, _, _] => Some((arch, &triple[(arch.len() + 1)..], "")),
+            [arch, _, _, env] => Some((
+                arch,
+                &triple[(arch.len() + 1)..(triple.len() - env.len() - 1)],
+                env,
+            )),
+            _ => None,
+        }
+    }
+
+    let mut archs = BTreeSet::new();
+    let mut oses = BTreeSet::new();
+    let mut envs = BTreeSet::new();
+    for (arch, os, env) in Platform::ALL
+        .iter()
+        .filter_map(|p| parse_triple(p.target_triple))
+    {
+        archs.insert(arch);
+        oses.insert(os);
+        if !env.is_empty() {
+            envs.insert(env);
+        }
+    }
+
+    let dst = Path::new(&env::var("OUT_DIR").unwrap()).join("known_triples.rs");
+    let mut out_file = fs::File::create(dst)?;
+    write!(
+        out_file,
+        r#"//
+// This is genarated by `generate_known_triples()` in `build.rs`. Please do not modify.
+//
+"#,
+    )?;
+
+    writeln!(out_file, "static LIST_ARCHS: &[&str] = &[")?;
+    for arch in archs {
+        writeln!(out_file, r#"    "{arch}","#)?;
+    }
+    writeln!(out_file, "];")?;
+
+    writeln!(out_file, "static LIST_OSES: &[&str] = &[")?;
+    for os in oses {
+        writeln!(out_file, r#"    "{os}","#)?;
+    }
+    writeln!(out_file, "];")?;
+
+    writeln!(out_file, "static LIST_ENVS: &[&str] = &[")?;
+    for env in envs {
+        writeln!(out_file, r#"    "{env}","#)?;
+    }
+    writeln!(out_file, "];")?;
+
+    Ok(())
 }
 
 fn main() {
@@ -28,6 +123,12 @@ fn main() {
     }
     let target = env::var("TARGET").unwrap();
     println!("cargo:rustc-env=TARGET={target}");
+
+    // Generate known target triple segments.
+    if let Err(e) = generate_known_triples() {
+        eprintln!("Unable to generate known target triple segments: {e:?}");
+        std::process::abort();
+    }
 
     // Set linker options specific to Windows MSVC.
     let target_os = env::var("CARGO_CFG_TARGET_OS");
